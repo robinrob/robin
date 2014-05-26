e00aafbc2a30a0f3e309d7fcde918721bd5a7635
msl <msl@mercury.local>
2014-05-25 17:31:26 -0700

    No submodules.

--- deinit.rb
+++ deinit.rb
@@ -1,5 +1,25 @@
 #!/usr/bin/env ruby
 
+def deinit_all()
+  
+  CSV.foreach("repos.csv", :headers => true) do |csv_obj|
+  
+    deinit(csv_obj['Repo'])
+    
+  end
+  
+  system("rm .gitmodules")
+  
+end
+
+
+def deinit(repo)
+  system("sudo rm -rf #{repo}")
+  # system("git rm -r --cached #{repo}")
+  system("git submodule deinit #{repo}")
+end
+
+
 require 'csv'
 
 if ARGV.size() == 2
@@ -9,20 +29,3 @@ else
 end
 
 
-def deinit_all()
-  
-  CSV.foreach("repos.csv", :headers => true) do |csv_obj|
-  
-    deinit_repo(repo)
-    system("rm .gitmodules")
-     
-  end
-  
-end
-
-
-def denit(repo)
-  system("sudo rm -rf #{repo}")
-  # system("git rm -r --cached #{repo}")
-  system("git submodule deinit #{repo}")
-end
