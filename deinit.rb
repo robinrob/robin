c7cf68c1dbd6d8a9dc8df1fb92fa0501e1c9f8bd
msl <msl@mercury.local>
2014-05-25 17:12:45 -0700

    Auto-update

--- deinit.rb
+++ deinit.rb
@@ -2,14 +2,23 @@
 
 require 'csv'
 
-CSV.foreach("repos.csv", :headers => true) do |csv_obj|
+if ARGV.size() == 2
+  repo = ARGV[1]
+end
+
+
+def deinit_all()
   
-  repo = csv_obj['Repo']
-  url = csv_obj['URL']
-  branch = csv_obj['Branch']
+  CSV.foreach("repos.csv", :headers => true) do |csv_obj|
   
+    deinit_repo(repo)
+    system("rm .gitmodules")
+  
+end
+
+
+def denit(repo)
   system("rm -rf #{repo}")
   # system("git rm -r --cached #{repo}")
   system("git submodule deinit #{repo}")
-  
 end
